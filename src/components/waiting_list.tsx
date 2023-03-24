0a4481b22e4fded8e8468eda429bc066f0e9743e
PushToTalk <x_killer90@mail.ru>
2023-03-24 18:55:38 +0300

    Оформил список заявок, начал работать с Websocket

--- src/components/waiting_list.tsx
+++ src/components/waiting_list.tsx
@@ -1,41 +1,116 @@
-import React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import React, { useState } from "react";
+import {
+  DataGrid,
+  GridColDef,
+  GridCellParams,
+  GridRenderCellParams,
+} from "@mui/x-data-grid";
+import { Box, Typography } from "@mui/material";
+
+function RenderDate(props: GridRenderCellParams<any>) {
+  const { value } = props;
+  const newValue = value.split(" ");
+
+  return (
+    <p>
+      {newValue[0]}
+      <br />
+      {newValue[1]}
+    </p>
+  );
+}
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 50 },
-  { field: "creationTime", headerName: "Creation time", width: 130 },
-  { field: "changeTime", headerName: "Change time", width: 130 },
+  {
+    field: "id",
+    headerName: "ID",
+    width: 70,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
+  },
+  {
+    field: "creationTime",
+    headerName: "Creation time",
+    width: 210,
+    renderCell: RenderDate,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
+  },
+  {
+    field: "changeTime",
+    headerName: "Change time",
+    width: 210,
+    renderCell: RenderDate,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
+  },
   {
     field: "status",
     headerName: "Status",
     type: "string",
-    width: 70,
+    width: 100,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
   },
   {
     field: "side",
     headerName: "Side",
     description: "This column has a value getter and is not sortable.",
     sortable: false,
-    width: 70,
+    width: 80,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
   },
-  { field: "price", headerName: "Price", width: 100 },
-  { field: "amount", headerName: "Amount", width: 150 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 90,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
+  },
+  {
+    field: "amount",
+    headerName: "Amount",
+    width: 100,
+    headerClassName: "columnBorder",
+    headerAlign: "center",
+    cellClassName: "row columnBorder",
+  },
   {
     field: "instrument",
     headerName: "Instrument",
     description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 150,
+    headerAlign: "center",
+    headerClassName: "columnBorder",
+    cellClassName: "row columnBorder ",
   },
 ];
 
 const rows = [
   {
     id: 1,
-    creationTime: "Snow",
-    changeTime: "Jon",
-    status: "Fill",
+    creationTime: "2022-01-01 12:00:01.321431",
+    changeTime: "2022-01-01 12:00:01.321431",
+    status: "Filled",
     side: "Buy",
+    price: "8.662",
+    amount: "50000",
+    instrument: "RUB/USD",
+  },
+  {
+    id: 2,
+    creationTime: "2022-01-01 12:00:01.321431",
+    changeTime: "2022-01-01 12:00:01.321431",
+    status: "Filled",
+    side: "Sell",
     price: "8.662",
     amount: "50000",
     instrument: "RUB/USD",
@@ -44,9 +119,62 @@ const rows = [
 
 function WaitingList() {
   return (
-    <div style={{ height: "90vh", width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100%",
+        "& .sell": {
+          color: "red",
+        },
+        "& .buy": {
+          color: "green",
+        },
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        autoHeight
+        getCellClassName={(params: GridCellParams<any, any, string>) => {
+          if (params.field !== "side") {
+            if (params.field !== "price") {
+              if (params.field !== "amount") {
+                return "";
+              }
+            }
+          }
+          return params.row.side.toLowerCase();
+        }}
+        sx={{
+          border: "0px solid black",
+          marginTop: "10px",
+          alignItems: "center",
+          "& .row": {
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            borderTop: "1px solid black",
+          },
+          "& .MuiDataGrid-columnHeaderTitle": {
+            fontSize: "18px",
+            fontWeight: "bold",
+          },
+          "& .columnBorder": {
+            borderRight: "1px solid black",
+          },
+          "& .MuiDataGrid-main": {
+            borderTop: "1px solid black",
+            borderBottom: "1px solid black",
+            borderLeft: "1px solid black",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderBottom: "1px solid black",
+            borderLeft: "1px solid black",
+            borderRight: "1px solid black",
+          },
+        }}
+      />
+    </Box>
   );
 }
 
